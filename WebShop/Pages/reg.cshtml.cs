f502d327a9d34bd61222dd52001060cb2c69c4c6
NekoFerris <nekoandreas@gmail.com>
2023-11-06 22:34:08 +0100

    sql

--- WebShop/Pages/reg.cshtml.cs
+++ WebShop/Pages/reg.cshtml.cs
@@ -5,8 +5,15 @@ namespace WebShop.Pages
 {
     public class regModel : PageModel
     {
+        public Kunde Kunde { get; set; } = new();
+
         public void OnGet()
         {
+        }
+
+        public void OnPostReg()
+        {
+            Kunde.Anlegen();
         }
     }
 }
