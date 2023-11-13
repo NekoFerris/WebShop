89f83616ecb529bdb3eee4e9f93b124c8c504209
CatgirlFerris <nekoandreas@gmail.com>
2023-11-13 15:59:15 +0100

    ??

--- WebShop/Pages/asuche.cshtml.cs
+++ WebShop/Pages/asuche.cshtml.cs
@@ -1,11 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
 
 namespace WebShop.Pages.Shared
 {
     public class asucheModel : PageModel
     {
         public List<Artikel> artikels = new();
+        public string korrecktur = "";
 
         public void OnGet()
         {
@@ -16,8 +17,21 @@ namespace WebShop.Pages.Shared
         {
             if (suchb != null)
                 artikels = Artikel.AlleLesen(suchb);
+            if (artikels.IsNullOrEmpty())
+            {
+                List<Artikel> _lstArt = new();
+                List<int> _lstGleichheit = new();
+                _lstArt = Artikel.AlleLesen();
+                foreach (Artikel a in _lstArt)
+                {
+
+                }
+                korrecktur = _lstArt[_lstGleichheit.IndexOf(_lstGleichheit.Max())].Bezeichnung.ToString();
+            }
             else
+            {
                 artikels = Artikel.AlleLesen();
+            }
         }
 
         public void OnPostAddArtikel(int artnr, int menge)
