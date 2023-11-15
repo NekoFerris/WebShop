e0c7bde640ddbcddcc6838a2f42d3fca681d182a
NekoFerris <nekoandreas@gmail.com>
2023-11-15 19:08:11 +0100

    update

--- WebShop/Pages/warenkorb.cshtml.cs
+++ WebShop/Pages/warenkorb.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.ObjectModel;
 using Microsoft.Extensions.Primitives;
 using System.Collections.Specialized;
+using System;
 
 namespace WebShop.Pages
 {
@@ -9,6 +10,7 @@ namespace WebShop.Pages
     {
         public Bestellung _bestellung = new();
         public List<Artikel> artikels = new();
+
         public void OnGet()
         {
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("lhash")))
@@ -20,6 +22,7 @@ namespace WebShop.Pages
                 Response.Redirect("/");
             }
         }
+
         public void OnPostAktuallesieren()
         {
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("lhash")))
@@ -28,26 +31,26 @@ namespace WebShop.Pages
                 ObservableCollection<BestellPos> bestellPos = new();
                 _bestellung = bestellung.Laden(Int32.Parse(HttpContext.Session.GetString("id")));
                 _bestellung.LstBestPoss = BestellPos.AlleLaden(Int32.Parse(HttpContext.Session.GetString("id")));
-                List<KeyValuePair<string, StringValues>> daten = Request.Query.Where(q => q.Key.Split(",")[0] == "menge").ToList(); ;
+                List<KeyValuePair<string, StringValues>> daten = HttpContext.Request.Form.Where(q => q.Key.Split(",")[0] == "menge").ToList(); ;
                 foreach (KeyValuePair<string, StringValues> valuePair in daten)
                 {
                     try
                     {
                         int id = Int32.Parse(valuePair.Key.Split(",")[1]);
-                        int mengeneu = Int32.Parse(valuePair.Value.FirstOrDefault());
-                        int mengealt = bestellung.LstBestPoss.Where(b => b.Id == id).Single().Anzahl;
+                        int mengeneu = Int32.Parse(valuePair.Value);
+                        int mengealt = _bestellung.LstBestPoss.Where(b => b.Id == id).Single().Anzahl;
                         if (mengeneu != mengealt && mengeneu > 0)
                         {
-                            bestellung.LstBestPoss.Where(b => b.Id == id).Single().Anzahl = mengeneu;
+                            _bestellung.LstBestPoss.Where(b => b.Id == id).Single().MengeAendern(mengeneu);
                         }
-                        else if(mengeneu == 0)
+                        else if (mengeneu == 0)
                         {
-                            bestellung.LstBestPoss.Remove(bestellung.LstBestPoss.Where(b => b.Id == id).Single());
+                            Bestellung.ArtikelEntfernen(Int32.Parse(HttpContext.Session.GetString("id")), id);
+                            Response.Redirect("/");
                         }
                     }
                     catch
                     {
-
                     }
                 }
                 TabelleFuellen();
@@ -57,6 +60,7 @@ namespace WebShop.Pages
                 Response.Redirect("/");
             }
         }
+
         public void TabelleFuellen()
         {
             Bestellung bestellung = new();
