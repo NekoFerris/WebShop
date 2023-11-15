bd8d5d3d623f1e8d9489f083d2b52f2ea76549c7
NekoFerris <nekoandreas@gmail.com>
2023-11-15 14:47:25 +0100

    warenkorb

--- WebShop/Pages/warenkorb.cshtml.cs
+++ WebShop/Pages/warenkorb.cshtml.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ShopBase.Persistenz;
 using System.Collections.ObjectModel;
+using Microsoft.Extensions.Primitives;
+using System.Collections.Specialized;
 
 namespace WebShop.Pages
 {
@@ -13,18 +13,59 @@ namespace WebShop.Pages
         {
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("lhash")))
             {
-                Bestellung bestellung = new();
-                ObservableCollection <BestellPos> bestellPos = new();
-                _bestellung = bestellung.Laden(Int32.Parse(HttpContext.Session.GetString("id")));
-                _bestellung.LstBestPoss = BestellPos.AlleLaden(Int32.Parse(HttpContext.Session.GetString("id")));
-                foreach(BestellPos pos in _bestellung.LstBestPoss)
-                {
-                    artikels.Add(Artikel.Lesen(pos.ArtikelId));
-                }
+                TabelleFuellen();
             }
             else
             {
                 Response.Redirect("/");
+            }
+        }
+        public void OnPostAktuallesieren()
+        {
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("lhash")))
+            {
+                Bestellung bestellung = new();
+                ObservableCollection<BestellPos> bestellPos = new();
+                _bestellung = bestellung.Laden(Int32.Parse(HttpContext.Session.GetString("id")));
+                _bestellung.LstBestPoss = BestellPos.AlleLaden(Int32.Parse(HttpContext.Session.GetString("id")));
+                List<KeyValuePair<string, StringValues>> daten = Request.Query.Where(q => q.Key.Split(",")[0] == "menge").ToList(); ;
+                foreach (KeyValuePair<string, StringValues> valuePair in daten)
+                {
+                    try
+                    {
+                        int id = Int32.Parse(valuePair.Key.Split(",")[1]);
+                        int mengeneu = Int32.Parse(valuePair.Value.FirstOrDefault());
+                        int mengealt = bestellung.LstBestPoss.Where(b => b.Id == id).Single().Anzahl;
+                        if (mengeneu != mengealt && mengeneu > 0)
+                        {
+                            bestellung.LstBestPoss.Where(b => b.Id == id).Single().Anzahl = mengeneu;
+                        }
+                        else if(mengeneu == 0)
+                        {
+                            bestellung.LstBestPoss.Remove(bestellung.LstBestPoss.Where(b => b.Id == id).Single());
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                TabelleFuellen();
+            }
+            else
+            {
+                Response.Redirect("/");
+            }
+        }
+        public void TabelleFuellen()
+        {
+            Bestellung bestellung = new();
+            ObservableCollection<BestellPos> bestellPos = new();
+            _bestellung = bestellung.Laden(Int32.Parse(HttpContext.Session.GetString("id")));
+            _bestellung.LstBestPoss = BestellPos.AlleLaden(Int32.Parse(HttpContext.Session.GetString("id")));
+            foreach (BestellPos pos in _bestellung.LstBestPoss)
+            {
+                artikels.Add(Artikel.Lesen(pos.ArtikelId));
             }
         }
     }
