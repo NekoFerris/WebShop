d8bb9ce157f8c5f9feb15d913d3f0242d1aac451
NekoFerris <nekoandreas@gmail.com>
2023-12-14 11:43:44 +0100

    startseite

--- WebShop/Pages/Index.cshtml.cs
+++ WebShop/Pages/Index.cshtml.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace WebShop.Pages
 {
     public class IndexModel : PageModel
     {
+        public List<Artikel> LstArtikel = null;
         public void OnGet()
         {
+            LstArtikel = Artikel.AlleLesen();
+            LstArtikel.Reverse();
+            LstArtikel = LstArtikel.Take(LstArtikel.Count > 10 ? 10 : LstArtikel.Count).ToList();
         }
     }
 }
